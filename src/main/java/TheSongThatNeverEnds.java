cbcbbc160b7d301accbd26a7f250eb837870a0c0
kristenrjackson <kristenrjackson7@gmail.com>
2021-03-31 17:40:07 -0400

    first commit - BeerSong and TheSongThatNeverEnds

---
+++ src/main/java/TheSongThatNeverEnds.java
@@ -0,0 +1,14 @@
+public class TheSongThatNeverEnds {
+    public static void main (String[] args) {
+        int repeatSong = 99;
+
+        while (repeatSong >= 0) {
+                System.out.println("This is the song that doesn't end.");
+                System.out.println("Yes, it goes on and on, my friend.");
+                System.out.println("Some people started singing it, not knowing what it was.");
+                System.out.println("And they'll continue singing it forever just because...");
+                System.out.println();
+                repeatSong--;
+        }
+    }
+}
