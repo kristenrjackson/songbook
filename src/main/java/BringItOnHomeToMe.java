c89688f5dd27583ef524b9da51616ffe1765511d
kristenrjackson <kristenrjackson7@gmail.com>
2021-04-01 06:58:28 -0400

    Added ComeAndGetYourLove and BringItOnHomeToMe

---
+++ src/main/java/BringItOnHomeToMe.java
@@ -0,0 +1,24 @@
+public class BringItOnHomeToMe {
+    public static void main (String[] args) {
+        String bring = "bring it to me";
+        String sweet = "Bring your sweet loving";
+        String home = "Bring it on home to me";
+        String yeah4 = "Yeah yeah yeah yeah";
+        String yeah6 = "Yeah yeah yeah yeah yeah yeah";
+
+        System.out.println("If you ever change your mind");
+        System.out.println("About leaving, leaving me behind");
+        System.out.println("Oh, oh " + bring);
+        System.out.println(sweet);
+        System.out.println(home);
+        System.out.println(yeah6);
+        System.out.println();
+        System.out.println("I know I laughed when you left");
+        System.out.println("But now I know I've only hurt myself");
+        System.out.println("Oh, oh " + bring);
+        System.out.println(sweet);
+        System.out.println(home);
+        System.out.println(yeah6);
+        System.out.println();
+    }
+}
---
+++ src/main/java/ComeAndGetYourLove.java
@@ -0,0 +1,41 @@
+public class ComeAndGetYourLove {
+    public static void main (String[] args) {
+        String hail = "Hail hail";
+        String chorus = "Come and get your love";
+        String la = "La la la la la la la la la la la la";
+
+        System.out.println(hail + ", what's the matter with your head, yeah");
+        System.out.println(hail + ", what's the matere with your mind and your sign and a oh oh oh");
+        System.out.println(hail + ", nothin' the matter with your head baby find it, come on and find it");
+        System.out.println("Hail, with it baby 'cause you're fine and you're mine and you look so divine");
+        System.out.println();
+        System.out.println(chorus);
+        System.out.println(chorus);
+        System.out.println(chorus);
+        System.out.println(chorus);
+        System.out.println();
+        System.out.println(hail + "what's the matter with you, feel right, don't you feel right baby");
+        System.out.println("Hail, oh yeah, get it from the mainline, all right");
+        System.out.println("I said I'll find it, find it");
+        System.out.println("Lovin', love it if you like it yeah");
+        System.out.println(hail + " it's your business");
+        System.out.println("If you want some, take some");
+        System.out.println("Get it together baby");
+        System.out.println();
+        System.out.println(chorus);
+        System.out.println(chorus);
+        System.out.println(chorus);
+        System.out.println(chorus);
+        System.out.println();
+        System.out.println(hail);
+        System.out.println("What's the matter with you, feel right, don't you feel right baby");
+        System.out.println("Hail, oh yeah");
+        System.out.println("Get it from the mainline, all right");
+        System.out.println();
+        System.out.println(la + " " + chorus);
+        System.out.println(la + " woo " + chorus);
+        System.out.println(la + " la la la la la la la " + chorus);
+        System.out.println("La da da da da da da da da ooh " + chorus);
+        System.out.println("La da bum bum bum da pa bum bum " + chorus);
+    }
+}
