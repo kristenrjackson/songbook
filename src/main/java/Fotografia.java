7e64cd257f7cfe17bd0b2cb31b28746d80ba018e
kristenrjackson <kristenrjackson7@gmail.com>
2021-04-03 07:22:21 -0400

    Fotografia and LovinTouchinSqueezin

---
+++ src/main/java/Fotografia.java
@@ -0,0 +1,36 @@
+public class Fotografia {
+    public static void main (String[] args) {
+        String distancia = "Y en la disancia te puedo ver \nCuando tus fotos me siento a ver \n" +
+                "Y en las estrellas tus ojos ver \nCuando tus fotos me siento a ver";
+        String busco = "Cada vez que te busco, te vas \nY cada vez que te llamo, no estas \nEs por eso que debo decir" +
+                "Que tu solo en mis fotos estas";
+
+        System.out.println();
+        System.out.println("Cada vez que you me voy \nLlevo a un lado de mi piel \nTus fotographias para verlas cada vez \n" +
+                "Que tu ausencia me devora entero el corazon \nY yo no tengo remedio mas que amarte");
+        System.out.println();
+        System.out.println(distancia);
+        System.out.println();
+        System.out.println(busco);
+        System.out.println();
+        System.out.println(busco);
+        System.out.println();
+        System.out.println("Cuando hay un abismo desnudo \nQue se opone entre los dos \n" +
+                "You me valgo del recuerdo taciturno de tu voz \nY de nuevo siento enfermo este corazon \n" +
+                "Que no le queda remedio mas que amarte");
+        System.out.println();
+        System.out.println(distancia);
+        System.out.println();
+        System.out.print("Hey ");
+        for (int i=0; i<8; i++) {
+            System.out.print("yeah ");
+        }
+        System.out.println();
+        System.out.println("Te puedo ver, yeah");
+        System.out.println();
+        for (int i=0; i<6; i++) {
+            System.out.println(busco);
+            System.out.println();
+        }
+    }
+}
---
+++ src/main/java/LovinTouchinSqueezin.java
@@ -0,0 +1,36 @@
+public class LovinTouchinSqueezin {
+    public static void main (String[] args) {
+        String na6 = "Na na na na na na";
+        String na5 = "Na na na na na";
+
+        System.out.println();
+        System.out.println("You make me weep, and want to die");
+        System.out.println("Just when you said we'd try");
+        System.out.println("Loving, touching, squeezing each other");
+        System.out.println("When I'm alone all by myself");
+        System.out.println("You're out with someone else");
+        System.out.println("Loving, touching, squeezing each other");
+        System.out.println();
+        System.out.println("You're tearing me apart");
+        System.out.println("Every, every day");
+        System.out.println("You're tearing me apart");
+        System.out.println("Oh, what can I say?");
+        System.out.println("You're tearing me apart");
+        System.out.println();
+        System.out.println("It won't e long, yes, 'til you're alone");
+        System.out.println("When your lover, oh, he hasn't come home");
+        System.out.println("'Cause he's loving, ooh, he's touching");
+        System.out.println("He's squeezing another");
+        System.out.println();
+        System.out.println("He's tearing you apart");
+        System.out.println("Ooh, every, every day");
+        System.out.println("Oh, girl, what can you say?");
+        System.out.println("Cause he's loving, touching another");
+        System.out.println();
+        System.out.println("Now it's your turn, girl, to cry");
+        for (int i = 0; i < 14; i++) {
+            System.out.println(na6);
+            System.out.println(na5);
+        }
+    }
+}
