34dc72f0f2ccf30f8f000792bae88b653d893ee8
kristenrjackson <kristenrjackson7@gmail.com>
2021-04-04 18:25:56 -0400

    SurfinBird and Kokomo

---
+++ src/main/java/Kokomo.java
@@ -0,0 +1,50 @@
+public class Kokomo {
+    public static void main (String[] args) {
+        String arubaFirst = "Aruba, Jamaica, oh I want to take ya \n" +
+                "Bermuda, Bahama, come on pretty mama \n" +
+                "Key Largo, Montego, baby why don't we go, Jamaica";
+        String aruba = "Aruba, Jamaica, oh I want to take you to \n" +
+                "Bermuda, Bahama, come on pretty mama \n" +
+                "Key Largo, Montego, baby why don't we go";
+        String kokomoWay = "Oh I want to take you down to \n" +
+                "Kokomo, we'll get there fast and then we'll take it slow \n" +
+                "That's where we want to go, way down in Kokomo";
+
+        System.out.println();
+        System.out.println(arubaFirst);
+        System.out.println();
+        System.out.println("Off the Florida Keys, there's a place called Kokomo \n" +
+                "That's where you wanna go to get away from it all \n" +
+                "Bodies in the sand, tropical drink melting in your hand \n" +
+                "We'll be falling in love to the rhythm of a steel drum band \n" +
+                "Down in Kokomo");
+        System.out.println();
+        System.out.println(aruba);
+        System.out.println(kokomoWay);
+        System.out.println();
+        System.out.println("Martinique, that Montserrat mystique");
+        System.out.println();
+        System.out.println("We'll put out to sea and we'll perfect our chemistry \n" +
+                "And by and by we'll defy a little bit of gravity \n" +
+                "Afternoon delight, cocktails and moonlit nights \n" +
+                "THat dreamy look in your eye, give me a tropical contact high \n" +
+                "Way down in Kokomo");
+        System.out.println();
+        System.out.println(aruba);
+        System.out.println(kokomoWay);
+        System.out.println();
+        System.out.println("Port au Prince, I want to catch a glimpse");
+        System.out.println();
+        System.out.println("Everybody knows a little place like Kokomo \n" +
+                "Now if you want to go to get away from it all \n" +
+                "Go down to Kokomo");
+        System.out.println();
+        System.out.println(aruba);
+        System.out.println(kokomoWay);
+        System.out.println();
+        System.out.println(aruba);
+
+
+
+    }
+}
---
+++ src/main/java/SurfinBird.java
@@ -0,0 +1,71 @@
+public class SurfinBird {
+    public static void main(String[] args) {
+        String everybody = "A-well-a everybody's heard about the bird";
+        String bird3b = "A-well, a bird, bird, bird, b-bird's the word";
+        String wella = "A-well, a bird, bird, bird, bird, well-a bird is the word";
+        String dont = "A-well-a don't you know about the bird?";
+        String ma2pa2 = "Ma-ma-mow, pa-pa, ma-ma-mow, pa-pa";
+        String ma4pa2 = "Ma-ma-ma-ma-mow, pa-pa, ma-ma-mow, pa-pa";
+        String ma2pa1 = "Ma-ma-mow, ma-ma-mow, pa-pa";
+        String ma2 = "Ma-ma-ma-ma, ma-ma-mow";
+        String well = "Well, everybody knows that the bird is the word";
+
+        System.out.println();
+        System.out.println(everybody);
+        System.out.println("B-b-b-bird, b-birdd's the word");
+        System.out.println("A-well, a bird, bird, bird, bird is the word");
+        System.out.println(wella);
+        System.out.println(bird3b);
+        System.out.println("A-well, a bird, bird, bird, well-a bird is the word");
+        System.out.println("A-well, a bird, bird, b-bird is the word");
+        System.out.println(wella);
+        System.out.println("A-well, a bird, bird, b-bird's the word");
+        System.out.println(dont);
+        System.out.println(well);
+
+        System.out.println();
+        System.out.println(everybody);
+        System.out.println("Bird, bird, bird, b-bird's the word");
+        System.out.println(bird3b);
+        System.out.println(bird3b);
+        System.out.println("A-well, a bird, bird, b-bird's the word");
+        for (int i = 0; i < 4; i++) {
+            System.out.println(bird3b);
+        }
+        System.out.println(dont);
+        System.out.println("Well, everybody's talking about the bird!");
+        System.out.println();
+        System.out.println("A-well-a bird, bird, b-bird's the word");
+        System.out.println("A-well-a bird, surfing bird, brr, brr, ah, ah");
+        System.out.print("Ah, bap-a");
+        for (int i = 0; i < 12; i++) {
+            System.out.print("-pa");
+        }
+        System.out.println("-pap");
+        for (int i = 0; i < 6; i++) {
+            System.out.println(ma2pa2);
+        }
+        for (int i = 0; i <2; i++) {
+            System.out.println(ma4pa2);
+        }
+        for (int i = 0; i < 2; i++) {
+            System.out.println(ma2pa2);
+        }
+        for (int i = 0; i < 2; i++) {
+            System.out.println(ma2pa1);
+        }
+        for (int i = 0; i < 2; i++) {
+            System.out.println(ma2);
+        }
+        System.out.println(ma2pa2);
+        System.out.println("Ma-ma-mow, pa-pa, ma-ma-mow");
+        System.out.println();
+        System.out.println("A-well-a don't you know about the bird?");
+        System.out.println(well);
+        System.out.println();
+        System.out.println("A-well, a bird, bird, b-bird's the word");
+        System.out.println("A-well-a mow, mow, pa-pa, ma-ma-mow, pa-pa");
+        System.out.println("Ma-ma-mow, ma-ma, mow, pa-pa");
+        System.out.println();
+    }
+}
