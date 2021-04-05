99acaec8b027aa44edee59efa4556a935d5f4bf4
kristenrjackson <kristenrjackson7@gmail.com>
2021-04-04 22:06:59 -0400

    Added DontStopBelievin and IWillSurvive

---
+++ src/main/java/DontStopBelievin.java
@@ -0,0 +1,28 @@
+public class DontStopBelievin {
+    public static void main (String[] args) {
+        String strangers = "Strangers, waiting \nUp and down the boulevard \nTheir shadows \nSearchin' in the night" +
+                "\nStreetlights, people \nLivin' just to find emotion \nHidin' somewhere in the night";
+        String believin = "Don't stop believin' \nHold on to that feelin' \nStreetlight, people";
+
+        System.out.println();
+        System.out.println("Just a small town girl \nLivin' in a lonely world \n" +
+                "She took the midnight train goin' anywhere \nJust a city boy \nBorn and raised in South Detroit \n" +
+                "He took the midnight train goin' anywhere");
+        System.out.println();
+        System.out.println("A singer in a smokey room \nThe smell of wine and cheap perfume \n" +
+                "For a smile they can share the night \nIt goes on and on, and on, and on");
+        System.out.println();
+        System.out.println(strangers);
+        System.out.println();
+        System.out.println("Workin' hard to get my fill \nEverybody wants a thrill \n" +
+                "Payin' anything to roll the dice \nJust one more time \nSome will win \nSome will lose \n" +
+                "Some were born to sing the blues \nOh, the movie never ends \nIt goes on and on, and on, and on");
+        System.out.println();
+        System.out.println(strangers);
+        System.out.println();
+        System.out.println(believin);
+        System.out.println("Don't stop, believin' \nHold on \nStreetlights, people");
+        System.out.println();
+        System.out.println(believin);
+    }
+}
---
+++ src/main/java/IWillSurvive.java
@@ -0,0 +1,40 @@
+public class IWillSurvive {
+    public static void main (String[] args) {
+        String go = "Go on now, go, walk out the door \nJust turn around now\n 'Cause you're not welcome anymore \n" +
+                "Weren't you the one who tried to hurt me with goodbye? \nYou think I'd crumble? \n" +
+                "You think I'd lay down and die?";
+        String survive = "Oh no, not I, I will survive \nOh, as long as I know how to love, I know I'll stay alive \n" +
+                "I've got all my life to live \nAnd I've got all my love to give and I'll survive \n" +
+                "I will survive, hey, hey";
+
+        System.out.println();
+        System.out.println("At first I was afraid, I was petrified \n" +
+                "Kept thinking I could never live without you by my side \n" +
+                "But then I spent so many nights thinking how you did me wrong \nAnd I grew strong" +
+                "And I learned how to get along \nAnd so you're back \nFrom outer space \n" +
+                "I just walked in to find you here with that sad look upon your face \n" +
+                "I should have changed that stupid lock, I should have made you leave your key \n" +
+                "If I'd known for just one second you'd be back to bother me");
+        System.out.println();
+        System.out.println(go);
+        System.out.println();
+        System.out.println(survive);
+        System.out.println();
+        System.out.println("It took all the strength I had not to fall apart \n" +
+                "Kept trying hard to mend the pieces of my broken heart \n" +
+                "And I spent oh-so many nights just feeling sory for myself \n" +
+                "I used to cry \nBut now I hold my head up high and you see me \nSomebody new" +
+                "I'm not that chained-up little person still in love with you \n" +
+                "And so you felt like dropping in and just expect me to be free \n" +
+                "Well, now I'm saving all my lovin' for someone who's loving me");
+        System.out.println();
+        System.out.println(go);
+        System.out.println();
+        System.out.println(survive);
+        System.out.println();
+        System.out.println("Oh");
+        System.out.println(go);
+        System.out.println();
+        System.out.println(survive);
+    }
+}
