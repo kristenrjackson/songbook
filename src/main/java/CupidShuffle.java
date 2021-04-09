28885d18b1459ce89e3ca576b55895e611506dc6
kristenrjackson <kristenrjackson7@gmail.com>
2021-04-09 00:42:12 -0400

    Added the rest of the CupidShuffle song

--- src/main/java/CupidShuffle.java
+++ src/main/java/CupidShuffle.java
@@ -32,5 +32,115 @@ public class CupidShuffle {
         System.out.println(" (now let me see ya do that dance)");
         System.out.println("New style, yeah");
         System.out.println();
+        System.out.println("They say I'm a rapper, and I say no \n" +
+                "They say 'what you doing?' Try'n to do somebody cold! (hey) \nI just let the music come from my soul \n" +
+                "So all of my people can stay on the floor (ooh ooh) \nThey got a brand new dance (come on) \n" +
+                "You gotta move your muscle \nBrand new dance, it's called the Cupid Shuffle \n" +
+                "It don't matter if you're young or you're old (here we go) \nWe gone show you how it go (hey hey)");
+        System.out.println();
+        System.out.println(right);
+        System.out.println(left);
+        System.out.println(kick);
+        System.out.println(walk);
+        System.out.println("(Let me see you do)");
+        System.out.println();
+        System.out.print(dance);
+        System.out.print(" (");
+        for (int i = 0; i<3; i++) {
+            System.out.print("yeah ");
+        }
+        System.out.println("yeah)");
+        System.out.print(dance);
+        System.out.println(" (a let me see ya do the cupid)");
+        System.out.print(dance);
+        System.out.print(" (");
+        for (int i = 0; i<7; i++) {
+            System.out.print("whoa ");
+        }
+        System.out.println("yeah yeah)");
+        System.out.print(dance);
+        System.out.println(" (come on)");
+        System.out.print(dance);
+        System.out.println(" (let me see, let me see ya do the cupid)");
+        System.out.print(dance);
+        System.out.println(" (do the shuffle baby yeah)");
+        System.out.print(dance);
+        System.out.println(" (woah woah oh oh)");
+        System.out.print(dance);
+        System.out.println(" (hey hey hey hey yeah yeah)");
+        System.out.println();
+        System.out.println(right);
+        System.out.println(left);
+        System.out.println(kick);
+        System.out.println(walk);
+        System.out.println("(Let me see you do)");
+        System.out.println(right);
+        System.out.println(left);
+        System.out.println(kick);
+        System.out.println(walk);
+        System.out.println();
+        System.out.println("Now you see what I'm talkin' about! \nI represent for the dirty south \n" +
+                "Where we known for swinging out \nI'm gone show you what I'm talkin' about");
+        System.out.println();
+        System.out.print(dance);
+        System.out.println(" (hey, hey)");
+        System.out.print(dance);
+        System.out.println(" (a let me see ya do the cupid shuffle, the cupid, cupid shuffle)");
+        System.out.println(dance);
+        System.out.print(dance);
+        System.out.println(" (the cupid shuffle, the cupid, cupid shuffle)");
+        System.out.print(dance);
+        System.out.println(" (ooh, ooh, oh)");
+        System.out.print(dance);
+        System.out.println(" (yeah)");
+        System.out.print(dance);
+        System.out.println(" (ooh, ooh, oh oh oh yeah)");
+        System.out.print(dance);
+        System.out.println(" (here we go) (here we go)");
+        System.out.println();
+        System.out.println(right);
+        System.out.println(left);
+        System.out.println(kick);
+        System.out.println(walk);
+        System.out.println("(Now walk wit' it!");
+        System.out.println(right);
+        System.out.println(left);
+        System.out.println(kick);
+        System.out.println(walk);
+        System.out.println("(Here we go, hey!)");
+        System.out.println();
+        System.out.print("And do the cupid shuffle (");
+        for (int i = 0; i<4; i++) {
+            System.out.print("oh ");
+        }
+        System.out.println("oh)");
+        System.out.print("Cupid Shuffle (");
+        for (int i = 0; i<6; i++) {
+            System.out.print("ooh ");
+        }
+        System.out.println("ooh)");
+        System.out.println("It's ya boy Cupid C U P the I the D");
+        System.out.println("Mr. Fat on the track, got another hittin beat \n(here we go hey)");
+        System.out.println();
+        System.out.print(dance);
+        System.out.println(" (come on, come on)");
+        System.out.print(dance);
+        System.out.println(" (let me see you)");
+        System.out.print(dance);
+        System.out.println(" (do your dance!)");
+        System.out.print(dance);
+        System.out.println(" (hey, hey, hey)");
+        System.out.print(dance);
+        System.out.println(" (we got brand new dance)");
+        System.out.print(dance);
+        System.out.println(" (we got brand new sound)");
+        System.out.print(dance);
+        System.out.println(" (we got a brand new feel)");
+        System.out.print(dance);
+        System.out.println(" (we got a brand new style y'all)");
+        System.out.println();
+        System.out.print("You better watch how close we comin' (uh, uh) \nSo get ready for the brand new style \n" +
+                "You better watch how close we comin' \nYou got another other Mr. Fat (uh, uh yep)");
+        System.out.println();
     }
 }
