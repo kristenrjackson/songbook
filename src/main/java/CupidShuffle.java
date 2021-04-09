0b535d91a2433f4b693fb90abfa85ad68e9309a4
kristenrjackson <kristenrjackson7@gmail.com>
2021-04-08 23:56:50 -0400

    Beginning of CupidShuffle

---
+++ src/main/java/CupidShuffle.java
@@ -0,0 +1,36 @@
+public class CupidShuffle {
+    public static void main (String[] args) {
+        String cupid = "Cupid shuffle, cupid shuffle";
+        String dance = "Down, down, do your dance, do your dance";
+        String right = "To the right, to the right, to the right, to the right";
+        String left = "To the left, to the left, to the left, to the left";
+        String kick = "Now kick, now kick, now kick, now kick";
+        String walk = "Now walk it by yourself, now walk it by yourself";
+
+        System.out.println();
+        System.out.println("New Cupid!");
+        System.out.println("(Time for a change)");
+        System.out.println(cupid);
+        System.out.println(cupid);
+        System.out.println("New style");
+        System.out.println();
+        System.out.print(dance);
+        System.out.println(" (do the cupid dance)");
+        System.out.print(dance);
+        System.out.println(" (come on) (don't stop)");
+        System.out.print(dance);
+        System.out.println(" (we got a brand new dance)");
+        System.out.print(dance);
+        System.out.println(" (hey, hey!)");
+        System.out.print(dance);
+        System.out.println(" (we got a brand new dance)");
+        System.out.print(dance);
+        System.out.println(" (oh, oh, oh)");
+        System.out.print(dance);
+        System.out.println(" (yeah, yeah!)");
+        System.out.print(dance);
+        System.out.println(" (now let me see ya do that dance)");
+        System.out.println("New style, yeah");
+        System.out.println();
+    }
+}
