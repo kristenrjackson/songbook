1caf41a8d9a7dba0e537015fc1c8d8bd3dadc14a
kristenrjackson <kristenrjackson7@gmail.com>
2021-04-09 21:36:11 -0400

    Added WhyOnlyCallMeWhenYoureHigh and GiveItAway

---
+++ src/main/java/GiveItAway.java
@@ -0,0 +1,54 @@
+public class GiveItAway {
+    public static void main(String[] args) {
+        String water = "What I've got you've got to give it to your mama \n" +
+                "What I've got you've got to give it to your pappa \n" +
+                "What I've got you've got to give it to your daughter \n" +
+                "You do a little dance and then you dring a little water";
+        String away = "Give it away give it away give it away now";
+
+        System.out.println();
+        System.out.println(water);
+        System.out.println();
+        for (int i = 0; i < 3; i++) {
+            System.out.println("What I've got you've got to get it put it in you");
+        }
+        System.out.println("Reeling with the feeling don't stop continue");
+        System.out.println();
+        System.out.println("Realize I don't want to be a miser \nConfide with sly you'll be the wiser \n" +
+                "Young blood is the lovin' upriser \nHow come everybody want to keep it like the kaiser");
+        System.out.println();
+        for (int i = 0; i<3; i++) {
+            System.out.println(away);
+        }
+        System.out.println();
+        System.out.println("I can't tell if I'm a kingpin or a pauper \nGreedy little people in a sea of distress \n" +
+                "Keep your more to receive your less \nUnimpressed by material excess \n" +
+                "Love is free love me say hell yes");
+        System.out.println();
+        System.out.println("I'm a low brow but I rock a little know how \nNo time for the piggies or the hoosegow \n" +
+                "Get smart get down with the pow wow \nNever been a better time than right now");
+        System.out.println();
+        System.out.println("Bob Marley poet and a prophet \nBob Marley taugh me how to off it \n" +
+                "Bob Marley walkin' like he talk it \nGoodness me can't you see I'm gonna cough it");
+        System.out.println();
+        for (int i = 0; i<3; i++) {
+            System.out.println(away);
+        }
+        System.out.println();
+        System.out.println("Lucky me swimmin' in my ability \nDancin' down on life with agility \n" +
+                "Come and drink it up from my fertility \nBlessed with a bucket of lucky mobility");
+        System.out.println();
+        System.out.println("My mom I love her 'cause she love me \nLong gone are the tiems when she scrub me \n" +
+                "Feelin' good my brother gonna hug me \nDrink my juice young love chug-a-lug me");
+        System.out.println();
+        System.out.println("There's a river born to be a giver \nKeep you warm won't let you shiver \n" +
+                "His heart is never gonna wither \nCome on everybody time to deliver");
+        System.out.println();
+        for(int i = 0; i<3; i++) {
+            System.out.println(away);
+        }
+        System.out.println();
+        System.out.println(water);
+        System.out.println();
+    }
+}
---
+++ src/main/java/WhyOnlyCallMeWhenYoureHigh.java
@@ -0,0 +1,31 @@
+public class WhyOnlyCallMeWhenYoureHigh {
+    public static void main (String[] args) {
+        String three = "Now it's three in the mornin' and I'm tryin' to change your mind \n" +
+                "Left you multiple missed calls and to my message you reply \n" +
+                "'Why'd you only call me when you're high?' \n'Hi, why'd you only call me when you're high?";
+        String why = "Why'd you only ever call me when you're high?";
+
+        System.out.println();
+        System.out.println("The mirror's image, it tells me it's home time \n" +
+                "But I'm not finished 'cause you're not by my side \n" +
+                "And as I arrived I thought I saw you leavin', carryin' your shoes \n" +
+                "Decided that once again I was just dreamin' of bumpin' into you");
+        System.out.println();
+        System.out.println(three);
+        System.out.println();
+        System.out.println("Somewhere darker, talkin' the same shite \nI need a partner, well are you out tonight? \n" +
+                "It's harder and harder t get you to listen \nMore I get through the gears \n" +
+                "Incapable of makin' alright decisions, and havin' bad ideas");
+        System.out.println();
+        System.out.println(three);
+        System.out.println();
+        System.out.println("And I can't see you here, wonnderin' where am I \n" +
+                "It sort of feels like I'm runnin' out of time \nI haven't found all I was hopin' to find \n" +
+                "You said you gotta be up in the mornin' \nGonna have an early night \n" +
+                "And you're starin' to bore me, baby \nWhy'd you only call me when you're high?");
+        System.out.println();
+        for (int i=0; i<4; i++) {
+            System.out.println(why);
+        }
+    }
+}
