15716b3359b8dcea9b878b8532f99c92e2f35687
kristenrjackson <kristenrjackson7@gmail.com>
2021-04-10 22:07:54 -0400

    Added AroundTheWorld and TheRockafellerSkank

---
+++ src/main/java/songbook/AroundTheWorld.java
@@ -0,0 +1,25 @@
+package songbook;
+
+public class AroundTheWorld {
+	public static void main (String[] args) {
+		String around = "Around the world, around the world";
+		
+		System.out.println();
+		for (int i = 0; i<8; i++) {
+			System.out.println(around);
+		}
+		System.out.println();
+		for (int i = 0; i<16; i++) {
+			System.out.println(around);
+		}
+		System.out.println();
+		for (int i = 0; i<28; i++) {
+			System.out.println(around);
+		}
+		System.out.println();
+		for (int i = 0; i<20; i++) {
+			System.out.println(around);
+		}
+	}
+
+}
---
+++ src/main/java/songbook/TheRockafellerSkank.java
@@ -0,0 +1,65 @@
+package songbook;
+
+public class TheRockafellerSkank {
+	public static void main (String[] args) {
+		String funk = "Right about now \nThe funk soul brother \nCheck it out now \nThe funk soul brother";
+		String funbro ="Right about now \nThe funk soul brother \nCheck it out now \nThe fun- bro-";
+		String boutnow = "Right about now, 'bout now \n'Bout now, 'bout now, 'bout now, 'bout now";
+		String bout = "'Bout, 'bout, 'bout, 'bout, 'bout, 'bout, 'bout, 'bout";
+		String funkbro = "Funk, funk, funk, funk, funk, funk, bro, bro, bro, bro, bro";
+		String funksoulbro = "Right about now \nThe funk soul bro, bro, bro, bro";
+		String funksoul1 = "Right about now \nFunk soul brother";
+		String funksoulthe1 = "Right about now \nThe funk soul brother";
+		String rightaboutfunk = "Right about \nFunk \nRight, right";
+		
+		System.out.println();
+		for (int i = 0; i<5; i++) {
+			System.out.println(funk);
+			System.out.println();
+		}
+		System.out.println("Right about now \nThe funk soul brother");
+		System.out.println();
+		System.out.println("Right about now, 'bout now \n'Bout now, 'bout now");
+		System.out.println();
+		for (int i = 0; i<7; i++) {
+			System.out.println(funk);
+			System.out.println();
+		}
+		System.out.println(funbro);
+		System.out.println();
+		for (int i = 0; i<2; i++) {
+			System.out.println(funk);
+			System.out.println();
+		}
+		System.out.println(boutnow);
+		System.out.println();
+		System.out.println(bout);
+		System.out.println(funkbro);
+		System.out.println();
+		System.out.println(funk);
+		System.out.println();
+		System.out.println(funksoulbro);
+		System.out.println();
+		System.out.println(funksoul1);
+		for (int i = 0; i<3; i++) {
+			System.out.println(funk);
+			System.out.println();
+		}
+		System.out.println(funksoulthe1);
+		System.out.println();
+		for (int i = 0; i<4; i++) {
+			System.out.println(funk);
+			System.out.println();
+		}
+		for (int i = 0; i<10; i++) {
+			System.out.println(boutnow);
+			System.out.println();
+		}
+		System.out.println(rightaboutfunk);
+		System.out.println();
+		for (int i = 0; i<4; i++) {
+			System.out.println(funk);
+			System.out.println();
+		}
+	}
+}
