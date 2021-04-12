ba14d3e9729b16df9a484c1b8e71443269cc77aa
kristenrjackson <kristenrjackson7@gmail.com>
2021-04-11 23:46:59 -0400

    Added LondonBridge

---
+++ src/main/java/songbook/LondonBridge.java
@@ -0,0 +1,33 @@
+package songbook;
+
+public class LondonBridge {
+	public static void main(String[] args) {
+		String bridge = "London Bridge is falling down";
+		String falling = "Falling down, falling down";
+		String fair = "My fair lady";
+		
+		System.out.println();
+		System.out.println(bridge);
+		System.out.println(falling);
+		System.out.println(bridge);
+		System.out.println(fair);
+		System.out.println();
+		System.out.println("Build it up with iron bars \nIron bars, iron bars \n"
+				+ "Build it up with iron bars");
+		System.out.println(fair);
+		System.out.println();
+		System.out.println("Iron bars will bend and break \nBend and break, bend and break \n"
+				+ "Iron bars will bend and break");
+		System.out.println(fair);
+		System.out.println();
+		System.out.println("Build it up with gold and silver \nGold and silver, gold and silver \n"
+				+ "Build it up with gold and silver");
+		System.out.println(fair);
+		System.out.println();
+		System.out.println(bridge);
+		System.out.println(falling);
+		System.out.println(bridge);
+		System.out.println("M-y-f-a-i-r-l-a-d-y");
+		System.out.println();
+	}
+}
