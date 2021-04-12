b985aba4bad3eb4b19b497acc58e0b3936d6b904
kristenrjackson <kristenrjackson7@gmail.com>
2021-04-11 23:35:34 -0400

    Added ChainOfFools

---
+++ src/main/java/songbook/ChainOfFools.java
@@ -0,0 +1,62 @@
+package songbook;
+
+public class ChainOfFools {
+	public static void main (String[] args) {
+		String chain1 = "Chain, chain, chain";
+		String chain2 = "(Chain, chain, chain)";
+		String chain3 = "Chain, chain, cha-i-in";
+		String fools = "Chain of fools";
+
+		
+		System.out.println();
+		for (int i = 0; i<3; i++) {
+			System.out.println(chain1);
+			System.out.println(chain2);
+		}
+		System.out.println(fools);
+		System.out.println();
+		System.out.println("For five long years \nI thought you were my man \nBut I found out \n"
+				+ "I'm just a link in your chain");
+		System.out.println();
+		System.out.println("You got me where you want me \nI ain't nothin' but your fool \n"
+				+ "Ya treated me mean \nOh you treated me cruel");
+		System.out.println();
+		System.out.println(chain1);
+		System.out.println(chain2);
+		System.out.println(fools);
+		System.out.println();
+		System.out.println("Every chain, has got a weak link \nI might be weak yeah \n"
+				+ "But I'll give you strength \nOh, hey");
+		System.out.println();
+		System.out.println("You told me to leave you alone \nMy father said, 'Come on home' \n"
+				+ "My doctor said, 'Take it easy' \nOh but your lovin' is much too strong \n"
+				+ "I'm added to your");
+		System.out.println();
+		System.out.println(chain1);
+		System.out.println(chain2);
+		System.out.println(chain1);
+		System.out.println(chain2);
+		System.out.println(chain3);
+		System.out.println(chain2);
+		System.out.println(fools);
+		System.out.println();
+		System.out.println("One of these mornings \nThe chain is gonna break \n"
+				+ "But up until the day \nI'm gonna take all I can take, oh hey");
+		System.out.println();
+		System.out.println(chain1);
+		System.out.println(chain2);
+		System.out.println(chain1);
+		System.out.println(chain2);
+		System.out.println(chain3);
+		System.out.println(chain2);
+		System.out.println(fools);
+		System.out.println();
+		System.out.println(chain3);
+		System.out.println(chain1);
+		System.out.println(chain3);
+		System.out.println(fools);
+		System.out.println();
+		
+		
+	}
+}
