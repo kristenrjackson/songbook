eae41e1ab77c30197e8d3c80bcdb531154cd7f1f
kristenrjackson <kristenrjackson7@gmail.com>
2021-04-11 23:17:59 -0400

    Added Happy

--- src/main/java/songbook/AroundTheWorld.java
+++ src/main/java/songbook/AroundTheWorld.java
@@ -21,5 +21,4 @@ public class AroundTheWorld {
 			System.out.println(around);
 		}
 	}
-
 }
---
+++ src/main/java/songbook/Happy.java
@@ -0,0 +1,69 @@
+package songbook;
+
+public class Happy {
+	public static void main (String[] args) {
+		String happy = "Because I'm happy";
+		String roof = "Clap along if you feel like a room without a roof";
+		String truth = "Clap along if you feel like happiness is the truth";
+		String happiness = "Clap along if you know what happiness is to you";
+		String wanna = "Clap along if you feel like that's what you wanna do";
+		String down = "Bring me down";
+		String to = "To bring me down";
+		
+		System.out.println("Let me tell you now \nHere we go");
+		System.out.println();
+		System.out.println("It might seem crazy what I'm about to say \nSunshine she's here, you can take a break "
+				+ "\nI'm in a hot air balloon that can go to space \n"
+				+ "With the air, like I don't care baby by the way (come on)");
+		System.out.println();
+		System.out.println(happy);
+		System.out.println(roof);
+		System.out.println(happy);
+		System.out.println(truth);
+		System.out.println(happy);
+		System.out.println(happiness);
+		System.out.println(happy);
+		System.out.println(wanna + " (hey)");
+		System.out.println();
+		System.out.println("Here come bad news talking this and that (yeah) \n"
+				+ "Well, give me all you got, and don't hold it back (yeah) \n"
+				+ "Well, I should probably warn you, you'll be just fine (yeah) \n"
+				+ "No offense to you, don't waste your time \n(Here's why)");
+		System.out.println();
+		System.out.println(happy);
+		System.out.println(roof);
+		System.out.println(happy);
+		System.out.println(truth);
+		System.out.println(happy);
+		System.out.println(happiness);
+		System.out.println(happy);
+		System.out.println(wanna);
+		System.out.println();
+		System.out.println("Here go \n(Happy)");
+		System.out.println(down);
+		System.out.println("Can't nothing \n(happy)");
+		System.out.println(down);
+		System.out.println("My level's too high (happy)");
+		System.out.println(to);
+		System.out.println();
+		System.out.println("To bring me");
+		System.out.println("(Let me tell you now)");
+		System.out.println(down + " \nCan't nothing \n" + down);
+		System.out.println("His level's too high");
+		for (int i = 0; i<2; i++) {
+			System.out.println(to);
+		}
+		for (int i = 0; i<2; i++) {
+			System.out.println();
+			System.out.println(happy);
+			System.out.println(roof);
+			System.out.println(happy);
+			System.out.println(truth);
+			System.out.println(happy);
+			System.out.println(happiness);
+			System.out.println(happy);
+			System.out.println(wanna);
+			System.out.println();
+		}
+	}
+}
