56f032cf82f1ab43e4ee638c919c855dc8a119c6
kristenrjackson <kristenrjackson7@gmail.com>
2021-04-11 23:58:59 -0400

    JingleBells

---
+++ src/main/java/songbook/JingleBells.java
@@ -0,0 +1,23 @@
+package songbook;
+
+public class JingleBells {
+	public static void main (String[] args) {
+		System.out.println();
+		System.out.println("Dashing through the snow \nIn a one horse open sleigh \n"
+				+ "Over the fields we go \nLaughing all the way \nBells on bobtail ring \n"
+				+ "Making spirits bright \nWhat fun it is to laugh and sing \nA sleighing song tonight!");
+		System.out.println();
+		System.out.println("Oh, jingle bells, jingle bells \nJingle all the way \nOh! what fun it is to ride \n"
+				+ "In a one horse open sleigh, hey \nJingle bells, jingle bells \nJingle all the way"
+				+ "Oh! waht fun it is to ride \nIn a one horse open sleigh");
+		System.out.println();
+		System.out.println("Now the ground is white \nGo it while you're young \n"
+				+ "Take the girls tonight \nAnd sing this sleighing song \nJust get a bobtailed bay \n"
+				+ "Two forty as his speed \nHitch him to an open sleigh \nAnd crack! You'll take the lead");
+		System.out.println();
+		System.out.println("Jingle bells, jingle bells \nJingle all the way \nOh, what fun it is to ride \n"
+				+ "In a one horse open sleigh, hey \nJingle bells, jingle bells \nJingle all the way \n"
+				+ "Oh, what fun it is to ride \nIn a one horse open sleigh...");
+		System.out.println();
+	}
+}
